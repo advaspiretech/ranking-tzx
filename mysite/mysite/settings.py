863feb8e2781f982d5201242353b42d1e2d36fe6
advaspiretech <advaspire.tech@gmail.com>
2023-12-08 22:37:42 +0800

    commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,16 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR/'static'
+#STATIC_URL = 'static/'
+#STATIC_ROOT = BASE_DIR/'static'
 
 # # Default primary key field type
 # # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # #aws storage
 # AWS_QUERYSTRING_AUTH = False
@@ -141,18 +141,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 AWS_ACCESS_KEY_ID = 'DO0062KLCMGLR4MXADWG'
 AWS_SECRET_ACCESS_KEY = 'Em8xtZywAClFto6DjjeRaPbUa+0XKAPsxKpMrahyVq0'
 AWS_STORAGE_BUCKET_NAME = 'advaspire-spaces'
-AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+AWS_S3_ENDPOINT_URL = 'https://sgp1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
+    'CacheControl': 'max-age=86400',    
 }
-AWS_LOCATION = 'advaspire-spaces'
+AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
